a1f04c6ce7cc5eaab4b5b177a5a2ac7b76f09325
vamshi krishna <vamshig736@gmail.com>
2023-05-22 13:29:58 +0000

    list n key

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import UserProfile from './components/UserProfile/index'
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Vamshi',
+    role: 'software engineer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Vamshi',
+    role: 'software engineer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Vamshi',
+    role: 'software engineer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
